e701ce33e4e2afcd477d6f25a6b6461d5dacfbf1
narekhovhannisyan <narekgilmour@gmail.com>
2017-08-09 15:56:11 +0400

    root directory fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ const gulp = require('gulp')
 const utils = require('./utils')
 const Promise = require('bluebird')
 const removeTypes = require('./remove-types')
+const path = require('path')
 
-const source = './src'
+const root = path.resolve(__dirname).split('/node_modules')[0]
+const source = root + '\\src'
 
 gulp.task('default', () => {
   return utils.callRecursive(utils.recursiveScan(source, removeTypes.unflowAsync), 1000 * 30) //every half minute

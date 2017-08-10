4be22286b6be92a6dae18dfde23263915e6473ce
narekhovhannisyan <narekgilmour@gmail.com>
2017-08-10 14:03:23 +0400

    bugfix

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,19 @@ const root = path.resolve(__dirname).split("node_modules")[0]
 const source = root + '\src'
 
 gulp.task('flow-remover', () => {
-  try {
-    if (utils.dirExists(source) && utils.dirExists(source.replace('src', 'dist'))) {
-      console.log('Everything is ok with folders! :)')
-      return utils.callRecursive(utils.recursiveScan(source, removeTypes.unflowAsync), 1000 * 30) //every half minute
-    } else throw e 
-  } catch (e) {
-    console.error('One or both folders are missing!')
-  }
+   if (utils.dirExists(source) && utils.dirExists(source.replace('src', 'dist'))) {
+     console.log('Everything is ok with folders! :)')
+   } else {
+     console.log(utils.dirExists(source))
+     console.log(utils.dirExists(source.replace('src', 'dist')))
+   }
+  // try {
+  //   if (utils.dirExists(source) && utils.dirExists(source.replace('src', 'dist'))) {
+  //     console.log('Everything is ok with folders! :)')
+  //     // return utils.callRecursive(utils.recursiveScan(source, removeTypes.unflowAsync), 1000 * 30) //every half minute
+  //   } else throw e 
+  // } catch (e) {
+  //   console.error('One or both folders are missing!')
+  // }
 })
 

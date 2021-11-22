b06f965afba1aa2301eb186ce8beb258e53a6d1a
narekhovhannisyan <narekgilmour@gmail.com>
2021-11-22 18:20:46 +0400

    src: fix remove types order.

--- src/remove-types.js
+++ src/remove-types.js
@@ -7,21 +7,13 @@ const flowRemoveTypes = require('flow-remove-types')
 const utils = require('./utils')
 
 /**
- * Look if there is src folder. If there is such one does nothing, otherwise creates one.
- *  Then calls `recursiveScan` function for current `source` with `unflow` processor.
- * @param source The source to be scanned.
- * @returns {Promise.<*>}
- */
-const createAndScan = source => utils.lookupOrCreate(source.replace('src', 'dist'))
-  .then(utils.recursiveScan(source, unflow))
-
-/**
- * Writes processed file.
- * @param source The source of the file.
- * @param target The target path of the file.
+ * @private
+ * Writes processed file. If source is modified, then change file, otherwise do nothing!
+ * @param {String} source The source of the file.
+ * @param {String} target The target path of the file.
+ * @returns {void}
  */
 const removeFlowTypes = (source, target) => {
-  /* if source is modified, then change file, otherwise do nothing! */
   if (utils.checkLastModifiedDate(target) < utils.checkLastModifiedDate(source)) {
     const input = fs.readFileSync(source, 'utf8')
     const output = flowRemoveTypes(input, { pretty: true })
@@ -32,7 +24,7 @@ const removeFlowTypes = (source, target) => {
 
 /**
  * Checks if source is `js` file then calls `removeFlowType`, otherwise copies the file.
- * @param source The source of the file.
+ * @param {String} source The source of the file.
  * @returns {Promise.<*>}
  */
 const unflow = (source) => {
@@ -45,13 +37,23 @@ const unflow = (source) => {
       if (!utils.dirExists(target)) {
         return utils.createDirectory(target)
       }
-    } else if (fs.statSync(source).isFile() && path.extname(source) === ext) {
-      return removeFlowTypes(source, target)
-    } else {
-      return utils.copyFile(source, target)
     }
+
+    if (fs.statSync(source).isFile() && path.extname(source) === ext) {
+      return removeFlowTypes(source, target)
+    }
+
+    return utils.copyFile(source, target)
   })
 }
+
+/**
+ * Look if there is src folder. If there is such one does nothing, otherwise creates one.
+ *  Then calls `recursiveScan` function for current `source` with `unflow` processor.
+ * @param {String} source The source to be scanned.
+ */
+const createAndScan = (source) => utils.lookupOrCreate(source.replace('src', 'dist'))
+  .then(() => utils.recursiveScan(source, unflow))
 
 module.exports = {
   createAndScan
